711075da15abef77ba88d473aa5731828f814e70
Dünkel <94564762+mrtz1503@users.noreply.github.com>
2025-08-01 16:56:58 +0200

    Update Window.cpp

--- src/Window.cpp
+++ src/Window.cpp
@@ -3,6 +3,39 @@
 
 namespace wui {
 
+	enum class WindowFlags : long long{
+        Visible = 1 << 0,
+
+    };
+
+
+	struct WindowData {
+		int minWidth, minHeight;
+		int maxWidth, maxHeight;
+
+        int width, height;
+        int x, y;
+
+
+
+	};
+
+
+
+	class Window {
+	public:
+
+
+
+
+
+	private:
+
+		
+
+	};
+
+
 
 
 }
